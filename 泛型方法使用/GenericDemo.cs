1056c017359211b658ac538fbd115d7a778732e3
ImRan Huang <hyp117@whut.edu.cn>
2023-06-11 14:42:28 +0800

    feat:泛型反法练习代码 `

---
+++ 泛型方法使用/GenericDemo.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 泛型方法使用
+{
+    /// <summary>
+    /// 编写一个出栈入栈的泛型类
+    /// 先进后出
+    /// </summary>
+    public class Mystack<T>
+    {
+        //准备一个数组类型
+        private T[] stack;
+        //元素的位置
+        private int stackPoint;
+        //数据容量
+        private int size;
+
+        public Mystack(int size)
+        {
+            this.size = size;
+            this.stack = new T[size];
+            this.stackPoint = -1;//因为入栈时索引是从0开始  +1=0
+
+        }
+
+        /// <summary>
+        /// 入栈操作
+        /// </summary>
+        /// <param name="item"></param>
+        public void Push(T item)
+        {
+            if (stackPoint >= size)
+            {
+                Console.WriteLine("栈空间已满");
+            }
+            else
+            {
+                stackPoint++;
+                this.stack[stackPoint] = item;
+            }
+        }
+
+        public T Pop()
+        {
+            T date = this.stack[stackPoint];
+            stackPoint--;
+            return date;
+        }
+
+    }
+    #region 泛型中default关键字的使用
+
+    public class MyGenericClass<T1, T2>
+    {
+        private T1 date1;
+
+        private T2 date2;
+
+        public MyGenericClass()
+        {
+            date1 = default(T1);//如果T1是引用类型，赋值为空；如果是值类型则赋值默认值
+
+            date2 = default(T2);
+        }
+    }
+
+
+    #endregion
+
+    #region 泛型约束
+
+    public class MyGenericClass1<T1, T2, T3>
+        where T1 : struct//T1必须是值类型
+        where T2 : class //解释：T2必须是引用类型，class就是代表引用类型 
+        where T3 : new()//解释：T3这个类型中必须有一个无参数的构造方法，这个约束必需放到最后
+    {
+        /// <summary>
+        /// 定义两个属性
+        /// </summary>
+        public List<T2> ProductList { get; set; }
+
+        public T3 Publisher { get; set; }
+
+        public MyGenericClass1()
+        {
+            this.ProductList = new List<T2>();
+            this.Publisher = new T3();//如果泛型中没有规定这个类型有无参数构造方法是不能直接使用
+        }
+
+        public T2 BuyCouese(T1 position)
+        {
+            dynamic index = position;//dynam是在运行时解析对象类型
+            return this.ProductList[index];
+
+        }
+
+    }
+
+
+    #endregion
+
+    public class Course
+    {
+        public string CourseName { get; set; }
+        public int period { get; set; }
+    }
+
+    public class CourseTeacher
+    {
+        public string TeacherName { get; set; }
+        public int period { get; set; }
+        public int CourseCount { get; set; }
+
+        public CourseTeacher()
+        {
+        }
+
+
+    }
+
+}
---
+++ 泛型方法使用/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 泛型方法使用
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //TestMystack();
+            TestGeneric();
+            Console.WriteLine(Add<int>(1, 2));
+            Console.WriteLine(Add<double>(1.2, 2.3));
+         
+
+            Console.ReadLine();
+        }
+
+
+
+        public void Test()
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+        }
+
+        /// <summary>
+        /// 测试出栈跟入栈
+        /// </summary>
+        static void TestMystack()
+        {
+            //创建泛型对象的对象
+            Mystack<int> mystack = new Mystack<int>(5);
+
+            for (int i = 1; i <= 5; i++)
+            {
+                mystack.Push(i);
+
+               
+            }
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(mystack.Pop());
+                
+            }
+
+            Mystack<string> mystack1 = new Mystack<string>(5);
+            mystack1.Push("JAVA");
+            mystack1.Push("C#");
+            mystack1.Push("C++");
+            mystack1.Push("Phthon");
+            mystack1.Push("SQL");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(mystack1.Pop());
+
+            }
+
+        }
+
+        /// <summary>
+        /// 测试泛型约束
+        /// </summary>
+        static void TestGeneric()
+        {
+            MyGenericClass1<int, Course, CourseTeacher> myClass = new MyGenericClass1<int, Course, CourseTeacher>();
+
+            myClass.Publisher = new CourseTeacher { TeacherName = "常老师" };
+            myClass.ProductList = new List<Course> {
+            new Course{CourseName="C#全栈开发",period=4},
+              new Course{CourseName="JAVA开发",period=10},
+                new Course{CourseName="SQL语句",period=2}
+
+            };
+
+            //调用方法
+            var myCourse = myClass.BuyCouese(0);
+            //Console.WriteLine( "我购买的课程是："+myCourse.CourseName.ToString()+"学期："+myCourse.period+"主讲老师："+myClass.Publisher.TeacherName);
+
+            Console.WriteLine($"我购买的课程是：{myCourse.CourseName} 学期：{myCourse.period}  主讲老师: {myClass.Publisher.TeacherName} ");
+        }
+
+        static T Add<T>(T a,T b)
+            where T:struct
+        {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            return a1 + b1;
+        }
+
+    }
+}
